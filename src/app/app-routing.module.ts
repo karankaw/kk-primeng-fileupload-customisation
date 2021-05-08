80ef292ff41b8f177d575bc2231c48610d047bd8
karankaw <karankaw@hotmail.com>
2021-05-08 22:23:15 +0530

    1 file modified, 1 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { FileUploadDemoComponent } from './components/fileupload/fileuploaddemo.component';
+import { HomeComponent } from './home.component';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       {
+        path: '',
+        component: HomeComponent,
         path: '',
         loadChildren: () =>
           import('./components/fileupload/fileuploaddemo.module').then(
