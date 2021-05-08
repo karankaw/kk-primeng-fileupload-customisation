13c42b22e3c18f6587dc8333466235ba8d950880
karankaw <karankaw@hotmail.com>
2021-05-08 22:59:22 +0530

    2 files modified

--- src/app/components/fileupload/fileuploaddemo.component.ts
+++ src/app/components/fileupload/fileuploaddemo.component.ts
@@ -1,30 +1,40 @@
-import {Component} from '@angular/core';
-import {MessageService} from 'primeng/api';
+import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
-    templateUrl: './fileuploaddemo.html',
-    providers: [MessageService]
+  templateUrl: './fileuploaddemo.html',
+  providers: [MessageService]
 })
-export class FileUploadDemoComponent {
-    
-    uploadedFiles: any[] = [];
-    
-    constructor(private messageService: MessageService) {}
+export class FileUploadDemo {
+  uploadedFiles: any[] = [];
 
-    onUpload(event) {
-        for(let file of event.files) {
-            this.uploadedFiles.push(file);
-        }
-        
-        this.messageService.add({severity: 'info', summary: 'Success', detail: 'File Uploaded'});
+  constructor(private messageService: MessageService) {}
+
+  onUpload(event) {
+    for (let file of event.files) {
+      this.uploadedFiles.push(file);
     }
-    
-    onBasicUpload(event) {
-        this.messageService.add({severity: 'info', summary: 'Success', detail: 'File Uploaded with Basic Mode'});
-    }
-    
-    onBasicUploadAuto(event) {
-        this.messageService.add({severity: 'info', summary: 'Success', detail: 'File Uploaded with Auto Mode'});
-    }
-    
+
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Success',
+      detail: 'File Uploaded'
+    });
+  }
+
+  onBasicUpload(event) {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Success',
+      detail: 'File Uploaded with Basic Mode'
+    });
+  }
+
+  onBasicUploadAuto(event) {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Success',
+      detail: 'File Uploaded with Auto Mode'
+    });
+  }
 }
