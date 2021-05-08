f70686550eff03ba36eee3220b37b76764289bae
karankaw <karankaw@hotmail.com>
2021-05-08 23:01:27 +0530

    1 file modified

--- src/app/components/fileupload/fileuploaddemo.component.ts
+++ src/app/components/fileupload/fileuploaddemo.component.ts
@@ -5,36 +5,8 @@ import { MessageService } from 'primeng/api';
   templateUrl: './fileuploaddemo.html',
   providers: [MessageService]
 })
-export class FileUploadDemo {
+export class FileUploadDemoComponent {
   uploadedFiles: any[] = [];
 
-  constructor(private messageService: MessageService) {}
-
-  onUpload(event) {
-    for (let file of event.files) {
-      this.uploadedFiles.push(file);
-    }
-
-    this.messageService.add({
-      severity: 'info',
-      summary: 'Success',
-      detail: 'File Uploaded'
-    });
-  }
-
-  onBasicUpload(event) {
-    this.messageService.add({
-      severity: 'info',
-      summary: 'Success',
-      detail: 'File Uploaded with Basic Mode'
-    });
-  }
-
-  onBasicUploadAuto(event) {
-    this.messageService.add({
-      severity: 'info',
-      summary: 'Success',
-      detail: 'File Uploaded with Auto Mode'
-    });
-  }
+  constructor() {}
 }
