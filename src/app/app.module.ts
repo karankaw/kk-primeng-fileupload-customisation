47c190a3880a1436b964ca852da2660730c0784c
karankaw <karankaw@hotmail.com>
2021-05-08 22:17:49 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { FileUploadDemoModule } from './components/fileupload/fileuploaddemo.module';
 import { FileUploadDemoComponent } from './components/fileupload/fileuploaddemo.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule,
+    AppRoutingModule,
     FileUploadDemoModule
   ],
   declarations: [AppComponent],
