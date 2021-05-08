58f6aec3e3cab7ad37021ce1810ab18c576bd5db
karankaw <karankaw@hotmail.com>
2021-05-09 00:18:34 +0530

    2 files modified

--- src/app/components/fileupload/fileuploaddemo.component.ts
+++ src/app/components/fileupload/fileuploaddemo.component.ts
@@ -1,32 +1,45 @@
-import {Component} from '@angular/core';
-import {MessageService} from 'primeng/api';
+import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
-    templateUrl: './fileuploaddemo.html',
-    providers: [MessageService]
+  templateUrl: './fileuploaddemo.html',
+  providers: [MessageService]
 })
 export class FileUploadDemoComponent {
-    
-    uploadedFiles: any[] = [];
-    
-    constructor(private messageService: MessageService) {}
+  uploadedFiles: any[] = [];
 
-    onUpload(event) {
-        for(let file of event.files) {
-            this.uploadedFiles.push(file);
-        }
-        
-        this.messageService.add({severity: 'info', summary: 'Success', detail: 'File Uploaded'});
+  constructor(private messageService: MessageService) {}
+
+  onUpload(event) {
+    for (let file of event.files) {
+      this.uploadedFiles.push(file);
     }
-    
-    onBasicUpload(event) {
-      console.log("hi");
-        this.messageService.add({severity: 'info', summary: 'Success', detail: 'File Uploaded with Basic Mode'});
-              console.log("Bye");
-    }
-    
-    onBasicUploadAuto(event) {
-        this.messageService.add({severity: 'info', summary: 'Success', detail: 'File Uploaded with Auto Mode'});
-    }
-    
+
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Success',
+      detail: 'File Uploaded'
+    });
+  }
+
+  onBasicUpload(event) {
+    console.log('hi');
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Success',
+      detail: 'File Uploaded with Basic Mode'
+    });
+    console.log('Bye');
+  }
+
+  onBasicUploadAuto(event) {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Success',
+      detail: 'File Uploaded with Auto Mode'
+    });
+  }
+  myUploader(event) {
+    console.log(event.files);
+  }
 }
