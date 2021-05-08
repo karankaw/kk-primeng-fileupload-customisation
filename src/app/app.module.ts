38384863a97c579d09203d193850d7298a17167e
karankaw <karankaw@hotmail.com>
2021-05-08 23:03:34 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { FileUploadDemoModule } from './components/fileupload/fileuploaddemo.module';
-import { FileUploadDemoComponent } from './components/fileupload/fileuploaddemo.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home.component';
 
