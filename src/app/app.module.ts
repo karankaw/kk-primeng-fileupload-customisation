0d72f84b1770a6d2df7d782ef134155d56dce106
karankaw <karankaw@hotmail.com>
2021-05-09 07:04:11 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule }    from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
@@ -15,29 +15,24 @@ import { InputSwitchModule } from 'primeng/inputswitch';
 
 import { AppDemoActionsModule } from './app.demoactions.component';
 import { HomeComponent } from './home.component';
-
+import { AppMenuComponent } from './app.menu.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HomeComponent
-      ],
-    imports: [
-        FormsModule,
-        ReactiveFormsModule,
-        AppRoutingModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        AutoCompleteModule,
-        ButtonModule,
-        RadioButtonModule,
-        InputSwitchModule,
-        TooltipModule,
-        AppDemoActionsModule,
-    ],
-    providers: [
-        { provide: LocationStrategy, useClass: HashLocationStrategy },
-    ],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent, AppMenuComponent],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AutoCompleteModule,
+    ButtonModule,
+    RadioButtonModule,
+    InputSwitchModule,
+    TooltipModule,
+    AppDemoActionsModule
+  ],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
